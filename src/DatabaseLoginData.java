da04825ab8ecd1b108c1ccf9104a6edd1c810270
alexander.holmstro3 <alle020916@gmail.com>
2020-12-15 13:40:22 +0100

    Slut av lektionen

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,14 @@
+/**
+ * This is a class
+ * Created 2020-03-10
+ *
+ * @author Magnus Silverdal
+ */
+
+public class DatabaseLoginData {
+    static String user = "alle02";
+    static String password = "password";
+    static String port = "3306";
+    static String DBname = "te18";
+    static String DBURL = "";
+}
